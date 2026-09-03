1cc1eca3e3b9780fc35711b52ef333f74c04b579
Thoiba-Singh <blaze110055@gmail.com>
2026-09-03 20:26:43 +0530

    Variables

---
+++ 02_Data_Types.js
@@ -0,0 +1,22 @@
+// alert (3+3) // This function can't be used in node.js, only applicable in browser
+
+console.log("Blaze");
+
+let name = "Blaze"
+let age = 19
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => True/False
+// null => stand alone value
+// undefined => value is not defined
+// symbol = unique
+
+
+// object 
+
+console.log(typeof null); // undefined
+console.log(typeof null) // object
