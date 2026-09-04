d2e972875ac2add55b7ffdaba8fb1991f50e45ab
Thoiba-Singh <blaze110055@gmail.com>
2026-09-04 19:46:17 +0530

    Conversion Operations

---
+++ 03_Conversion_Operation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+let isloggedIn = "Blaze"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Blaze" => true
+
+let a = 33
+
+let A = String(a)
+console.log(A)
+console.log(typeof A)
