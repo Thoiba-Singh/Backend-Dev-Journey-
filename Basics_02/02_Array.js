a4ea9ecbbf0db9b396ce78106df3336aaaee4889
Thoiba-Singh <blaze110055@gmail.com>
2026-09-10 20:13:17 +0530

    Array_2

--- Basics_02/02_Array.js
+++ Basics_02/02_Array.js
@@ -23,9 +23,10 @@ const dc_heros = ["SuperMan", "Flash", "BatMan"]
 // console.log(Array.from("Blaze"));
 // console.log(Array.from({name = "Blaze"}));           // Interesting //
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
-console.log(Array.of(score1, score2, score3));
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1, score2, score3));
 
 
