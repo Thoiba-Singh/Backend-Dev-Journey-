ec29c9b458c397d8421fa56db380242d28d19d49
Thoiba-Singh <blaze110055@gmail.com>
2026-09-10 18:56:48 +0530

    Conversion_Operation

---
+++ Basics_01/03_Conversion_Operation.js
@@ -0,0 +1,60 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+let isloggedIn = "Blaze"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Blaze" => true
+
+let a = 33
+
+let A = String(a)
+// console.log(A)
+// console.log(typeof A)
+
+// ********************************** Operations **************************************
+
+let value = 1
+
+let negvalue =  -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Blaze"
+
+let str3 = str1 + " " + str2
+// console.log(str3);
+
+// console.log("1" + " " + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+// console.log(true+)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
