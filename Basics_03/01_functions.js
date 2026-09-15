f29d481bef4ba02674989f5d17649bd5237a119a
Thoiba-Singh <blaze110055@gmail.com>
2026-09-15 19:52:37 +0530

    function_Update

--- Basics_03/01_functions.js
+++ Basics_03/01_functions.js
@@ -27,7 +27,7 @@ const result = addTwoNumbers(3, 4)
 // console.log("Result: ", result)
 
 function loginUserMessage(username) {
-    if(username === undefined){                        /* People also use this(!username) */
+    if(username === undefined){                        /* People also use this (!username) */
         console.log("Please enter a username");
         return
     }
@@ -35,7 +35,36 @@ function loginUserMessage(username) {
 }
 
 // console.log(loginUserMessage("Blaze"));
+// console.log(loginUserMessage())
 
-console.log(loginUserMessage())
 
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200, 400, 500, 2000))
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Blaze",
+    price: 199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
